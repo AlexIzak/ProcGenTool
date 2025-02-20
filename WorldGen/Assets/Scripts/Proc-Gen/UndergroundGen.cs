ced03e14fcd9198568d3946bb20c511f54686bcb
Alex Izak <izak_alex@yahoo.com>
2025-02-20 13:21:02 +0000

    Good progress

--- WorldGen/Assets/Scripts/Proc-Gen/UndergroundGen.cs
+++ WorldGen/Assets/Scripts/Proc-Gen/UndergroundGen.cs
@@ -1,29 +1,33 @@
 using System;
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static SurfaceGen;
+
 
 public class UndergroundGen : MonoBehaviour
 {
     Dictionary<int, TileBase> tileset;
     Dictionary<int, GameObject> tileGroups;
-    //public Tilemap underground;
-    public TileBase hollow;
-    public TileBase dirt;
-    public TileBase rock;
+    
+    [Header("Based on how often they appear on the tilemap")]
+    public TileBase average;
+    public TileBase most;
+    public TileBase least;
 
+    [Header("Map dimensions")]
     [SerializeField] int width = 160;
     [SerializeField] int height = 90;
 
     [Range(4.0f, 20.0f)] //Recommended range
     [SerializeField] float magnification = 7.0f;
 
+    [field:System.ComponentModel.ReadOnly(true)]    
     [SerializeField] int xOffset = 0;
     [SerializeField] int yOffset = 0;
-
-    int seed = 0;
 
     List<List<int>> noiseGrid = new List<List<int>>();
     List<List<TileBase>> tileGrid = new List<List<TileBase>>();
@@ -31,11 +35,30 @@ public class UndergroundGen : MonoBehaviour
     //Background
     [SerializeField] SpriteRenderer bgWall;
 
+    [Serializable]
+    public struct WorldParams
+    {
+        //Size,
+        public int width;
+
+        [Unity.Collections.ReadOnly] public int depth;
+
+        [field: Unity.Collections.ReadOnly]
+        public float seed;
+
+        [Range(0f, 30f)]
+        public float smoothness;
+    
+        [Range(0f, 30f)]
+        public float heightValue;
+    }
+
+    [SerializeField]
+    private WorldParams worldParams;
+
     // Start is called before the first frame update
     void Start()
     {
-        xOffset = UnityEngine.Random.Range(-20, 20);
-        yOffset = UnityEngine.Random.Range(-20, 20);
 
         float xPos = width / 2;
         float YPos = height / 2;
@@ -45,16 +68,16 @@ public class UndergroundGen : MonoBehaviour
         bgWall.size = new Vector2(width, height);
 
         CreateTileset();
-        CreateTilemapGroups();
-        GenerateUnderground();
+
+        Generate();
     }
 
     private void CreateTileset()
     {
         tileset = new Dictionary<int, TileBase>();
-        tileset.Add(0, hollow);
-        tileset.Add(1, dirt);
-        tileset.Add(2, rock);
+        tileset.Add(0, average);
+        tileset.Add(1, most);
+        tileset.Add(2, least);
         //Add more later
     }
 
@@ -72,8 +95,31 @@ public class UndergroundGen : MonoBehaviour
         }
     }
 
+    //TODO Parallax mapping for the background
+
+    private void GenerateSurface()
+    {
+        worldParams.seed = UnityEngine.Random.Range(-100000, 100000);
+
+        //TODO - Create scriptable objects that have their own function to make biomes
+        for (int x = 0; x < worldParams.width; ++x)
+        {
+            float p = Mathf.PerlinNoise(x / worldParams.smoothness, worldParams.seed);
+            worldParams.depth = Mathf.RoundToInt(worldParams.heightValue * p + 90f);
+
+            //noise.pnoise
+            //noise.cellular
+
+            for (int y = 90; y < worldParams.depth; ++y)
+            {
+                tileGroups[2].GetComponent<Tilemap>().SetTile(new Vector3Int(x, y, 0), tileset[2]);
+            }
+        }
+    }
+
     private void GenerateUnderground()
     {
+        //TODO Add surface generation
         for (int x = 0; x < width; x++)
         {
             noiseGrid.Add(new List<int>());
@@ -116,12 +162,7 @@ public class UndergroundGen : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            xOffset = UnityEngine.Random.Range(-20, 20);
-            yOffset = UnityEngine.Random.Range(-20, 20);
-
-            CreateTileset();
-            CreateTilemapGroups();
-            GenerateUnderground();
+            Generate();
         }
         else if (Input.GetMouseButtonDown(1))
         {
@@ -130,7 +171,49 @@ public class UndergroundGen : MonoBehaviour
         }
     }
 
-    private void ClearMap()
+    public void Generate()
+    {
+        xOffset = UnityEngine.Random.Range(-20, 20);
+        yOffset = UnityEngine.Random.Range(-20, 20);
+
+        CreateTilemapGroups();
+        GenerateUnderground();
+        GenerateSurface();
+    }
+
+    public void SetData(int tileID)
+    {
+        tileset.Remove(0);
+
+        switch (tileID)
+        {
+            case 0:
+                tileset.Add(0, average);
+                break;
+
+            case 1:
+                tileset.Add(0, most);
+                break;
+
+            case 2:
+                tileset.Add(0, least);
+                break;
+
+            default:
+                tileset.Add(0, average);
+                break;
+        }
+
+        Debug.Log(tileID);
+    }
+
+    public void SwapTile(int tilesetID)
+    {
+        tileset.Remove(tilesetID);
+        tileset.Add(tilesetID, average);
+    }
+
+    public void ClearMap()
     {
         foreach(var group in tileGroups)
         {
