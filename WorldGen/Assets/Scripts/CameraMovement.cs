c0c03499a4e15748eef9c55e27e6a35dd600e4ef
Alex Izak <izak_alex@yahoo.com>
2025-03-22 18:29:12 +0000

    Camera Controls

---
+++ WorldGen/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cam;
+
+    [SerializeField]
+    int camSpeed = 1;
+
+    private void Awake()
+    {
+        //cam = GetComponent<Camera>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float x = cam.transform.position.x;
+        float y = cam.transform.position.y;
+
+        x += Input.GetAxis("Horizontal") * camSpeed;
+        y += Input.GetAxis("Vertical") * camSpeed;
+
+        cam.transform.position = new Vector3(x, y, -1);
+
+        if(Input.GetKey(KeyCode.KeypadPlus)) cam.GetComponent<Camera>().orthographicSize += 1;
+        if (Input.GetKey(KeyCode.KeypadMinus)) cam.GetComponent<Camera>().orthographicSize -= 1;
+    }
+}
